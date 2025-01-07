47fe98eaa62aa920083387892d40ac3574e16de5
nanxuan sun <nanxuansun@nanxuandeMacBook-Air.local>
2025-01-07 09:38:40 -0800

    v6.2.0

--- web/context_processors.py
+++ web/context_processors.py
@@ -1,8 +1,5 @@
 from web import models
 
 def navbar_context_processor(request):
-    if request.user.is_authenticated:
-        unread_message_count = models.Message.objects.filter(receiver=request.unicom_userid, is_read=False).count()
-    else:
-        unread_message_count = 0
+    unread_message_count = models.Message.objects.filter(receiver=request.unicom_userid, is_read=False).count()
     return {'unread_message_count': unread_message_count}
